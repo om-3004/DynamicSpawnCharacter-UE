1d89aead3b9b4c6f77487e51b96498f69a8b3e08
om-3004 <omthakkar3004@gmail.com>
2024-04-24 17:47:22 +0530

    Complete part1

---
+++ Source/PawnPossession/PawnPossessionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawnPossessionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWNPOSSESSION_API APawnPossessionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
